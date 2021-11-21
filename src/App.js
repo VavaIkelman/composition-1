9b1011bd50b36dab1c6ae985b5eb13d57ec55b37
VavaIkelman <mr.ilkevich@gmai.com>
2021-11-21 14:41:37 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Cards from './components/Cards';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Cards image="">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#1" className="btn btn-primary">Go somewhere</a>
+      </Cards>
+      <Cards>
+        <h5 className="card-title">Card title</h5>
+        <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#1" className="card-link">Card link</a>
+        <a href="#2" className="card-link">Another link</a>
+      </Cards>
     </div>
   );
 }
